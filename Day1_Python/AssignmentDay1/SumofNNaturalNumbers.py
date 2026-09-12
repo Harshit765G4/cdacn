908ff5956ffe2b4ca20cefcdb2941923744c18b3
Hey_Nerd <gargharshit765@gmail.com>
2026-09-13 02:17:44 +0530

    File Name Format And Code Bugs Fixes

--- Day1_Python/AssignmentDay1/SumofNNaturalNumbers.py
+++
@@ -1,4 +0,0 @@
-num = int(input("enter a number: "))
-
-result = int(num*(num+1)/2)
-print(result)
--- Day1_Python/AssignmentDay2/EmailDomainExtractor.py
+++
@@ -1,8 +0,0 @@
-email = input('Enter your Email Address: ')
-
-if not email.__contains__("@"):
-    print('Invalid Email')
-else:
-    str = email.split("@")
-    print(str[1])
-
