e704759c62ec05207210b3bd97872be939ef24bd
Hey_Nerd <gargharshit765@gmail.com>
2026-09-13 02:18:25 +0530

    File Name Format And Code Bugs Fixes

--- Day1_Python/AssignmentDay3/TheWizard'sMagicBag.py
+++
@@ -1,9 +0,0 @@
-list=["staff", "potion", "spellbook"]
-item=input("Enter the item:")
-list.append(item)
-
-print("Portal transition activated!")
-print(f"Ejected oldest item: {list[0]}")
-first=list[0]
-list.remove(first)
-print(f"Current items in the magic bag: {list}")
